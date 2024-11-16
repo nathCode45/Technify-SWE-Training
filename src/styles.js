b680a43912add78f89e6df6a8f0111712f74a46b
Amanda Nemshin <amanda.nemshin@duke.edu>
2024-11-16 13:28:14 -0500

    football

---
+++ src/styles.js
@@ -0,0 +1,33 @@
+export const containerStyle = {
+  display: 'flex',
+  alignItems: 'center', 
+  justifyContent: 'flex-start',
+  gap: '10px'
+};
+
+export const containerStyle2 = {
+  display: 'flex',
+  alignItems: 'center', 
+  justifyContent: 'flex-start',
+  gap: '60px'
+};
+
+export const buttonImageStyle = {
+  width: '400px',
+  height: '550px',
+  cursor: 'pointer',
+  marginLeft: '150px',
+
+};
+
+export const paragraphStyle = {
+  fontSize: '20px',
+  color: '#595959',
+  textAlign: 'left',
+  marginTop: '20px',
+  marginLeft: '60px',
+  marginRight: '60px'
+};
+
+
+
